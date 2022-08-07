30de183dd95507f6f9fe9163b4f6895cb9ed141b
Lovishchugh01 <lovishchugh01@gmail.com>
2022-08-07 17:14:22 +0530

    Third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,19 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-dark navbar-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">NewsStack</a>
+                <a className="navbar-brand text-light" href="/">NewsStack</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    <a className="nav-link active text-light" aria-current="page" href="/">Home</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
+                    <a className="nav-link text-light" href="/about">About</a>
                     </li>
                     {/* <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} =this.props;
+    let {title, description, imageUrl, newsUrl} =this.props;
     return (
       <div>
-        <div className="card" style={{width:'18rem'}}>
-            <img src="..." className="card-img-top" alt="..."/>
+        <div className="card m-3" style={{width:'18rem'}}>
+            <img src={imageUrl?imageUrl:"https://i0.wp.com/electrek.co/wp-content/uploads/sites/3/2020/10/Tesla-Autopilot-Radar-e1603395743425.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1"} className="card-img-top" height={'250px'} alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{description}...</p>
+                <a rel="noreferrer" href={newsUrl} target="_blank"className="btn btn-dark">Read More</a>
             </div>
         </div>
       </div>
